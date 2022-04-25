b21d933ad564f2739ba530cb6092ed1d2afd80df
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-04-25 16:10:42 -0300

    rearranged functions in object

--- game.js
+++ game.js
@@ -1,32 +1,39 @@
-let game = {
-  board: {
+let board = {
     width: 10,
     height: 5,
+    mines: 10,
     array: [],
-    drawBoard(){
-      for(let y = 0; y < this.height; y++){
-        this.array.push([]);
-        for(let x = 0; x < this.width; x++){
-          this.array[y].push(0);
-        };
+  drawBoard(){
+    for(let y = 0; y < this.height; y++){
+      this.array.push([]);
+      for(let x = 0; x < this.width; x++){
+        this.array[y].push(0);
       };
-    },
+    };
+    console.table(this.array); //for debugging
   },
-  mines: {
-    quantity: 10,
-    rand(num){
-      return parseInt(Math.floor(Math.random()*num));
-    },
-    placeMines(){
-      for(let i = 0; i < this.quantity; i++){
-        let x = this.rand(game.board.width);
-        let y = this.rand(game.board.height);
-        game.board.array[y][x] = "m";
-      };
-      console.table(game.board.array);
-    },
+  rand(num){
+    return parseInt(Math.floor(Math.random()*num));
+  },
+  placeMines(){
+    for(let i = 0; i < this.mines; i++){
+      let x, y;
+      do {
+        x = this.rand(this.width);
+        y = this.rand(this.height);
+      } while(this.array[y][x] == "m");
+      this.array[y][x] = "m";
+    };
+    console.table(this.array); //for debugging
+  },
+  validPosition(x,y){
+    if(x >= 0 && x < this.width){
+      console.log([x,y]);
+      console.log(width);
+      return [x,y];
+    };
   },
 };
 
-game.board.drawBoard();
-game.mines.placeMines();
+board.drawBoard();
+board.placeMines();
