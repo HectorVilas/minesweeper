6875216ee8ac98b1ada1b3acc381237e5d6dc171
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-04-25 21:33:10 -0300

    code for drawing board on dom

--- game.js
+++ game.js
@@ -1,14 +1,34 @@
 let board = {
-    width: 10,
-    height: 5,
-    mines: 10,
-    array: [],
-  drawBoard(){
+  dom: document.querySelector(".board"),
+  width: 20,
+  height: 15,
+  mines: 30,
+  array: [],
+  newGame(){
+    this.arrayBoard();
+    this.placeMines();
+    this.drawBoard();
+  },
+  arrayBoard(){
     for(let y = 0; y < this.height; y++){
       this.array.push([]);
       for(let x = 0; x < this.width; x++){
         this.array[y].push(0);
       };
+    };
+  },
+  drawBoard(){
+    for (let i = 0; i < this.height; i++) {
+      let row = document.createElement("div");
+      row.className = "row";
+      for (let j = 0; j < this.width; j++) {
+        let tile = document.createElement("div");
+        tile.className = "tile";
+        tile.setAttribute("x", j);
+        tile.setAttribute("y", i);
+        row.append(tile);
+      };
+      this.dom.appendChild(row);
     };
   },
   randIndex(num){
@@ -36,13 +56,11 @@ let board = {
   validPosition(arr){
     if(arr[0] >= 0 && arr[0] < this.width
       && arr[1] >= 0 && arr[1] < this.height
-      && this.array[arr[1]][arr[0]] != "m"
-      ){
+      && this.array[arr[1]][arr[0]] != "m"){
       return arr;
     };
   },
 };
 
-board.drawBoard();
-board.placeMines();
+board.newGame();
 console.table(board.array); //for debugging
